9c5ce767730e96efd412c3f00164339bb3462574
ozeraltun <ozer.altun93@gmail.com>
2021-02-17 16:48:29 +0300

    AI detection improved

--- Assets/enemy.cs
+++ Assets/enemy.cs
@@ -5,20 +5,45 @@ using UnityEngine.AI;
 
 public class enemy : MonoBehaviour
 {
-    [SerializeField] Transform target;
+    [SerializeField] Transform target; //target is the player
+    [SerializeField] float beforeDetectRadius = 5f;
+    [SerializeField] float afterDetectRadius = 12f;
+    float detectRadius;
+    float distanceToTarget = Mathf.Infinity;
+    bool detected;
     NavMeshAgent navMeshAgent;
     
     // Start is called before the first frame update
     void Start()
     {
         navMeshAgent = GetComponent<NavMeshAgent>();
-
+        detectRadius = beforeDetectRadius;
+        detected = false;
     }
 
     // Update is called once per frame
     void Update()
     {
-        navMeshAgent.SetDestination(target.position);
+        detectionCheck();
+    }
+    void detectionCheck(){
+        //Calculate distance between enemy and player
+        distanceToTarget = Vector3.Distance(target.position, transform.position);
         
+        if(distanceToTarget<detectRadius){
+            navMeshAgent.SetDestination(target.position);
+            detectRadius = afterDetectRadius;
+            detected = true;    
+        }
+        else{
+            detected = false;
+            detectRadius = beforeDetectRadius;
+        }
+    }
+    void OnDrawGizmosSelected(){
+        Gizmos.color = Color.blue;
+        Gizmos.DrawSphere(transform.position, beforeDetectRadius);
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position, afterDetectRadius);
     }
 }
