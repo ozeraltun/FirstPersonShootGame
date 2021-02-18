7cfcc604c355b327a7a948d162e04ec18eca76ed
Ozer Altun <ozer.altun93@gmail.com>
2021-02-18 13:12:51 +0300

    We can hit objects

---
+++ Assets/weapon.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class weapon : MonoBehaviour
+{
+    [SerializeField] Camera FPCamera;
+    [SerializeField] float range = 100f;
+
+    void Update(){
+        if (Input.GetButtonDown("Fire1")){
+            Shoot();
+        }
+    }
+    private void Shoot(){
+        RaycastHit hit; //What you hit
+        Physics.Raycast(FPCamera.transform.position, FPCamera.transform.forward, out hit, range); //position direction whatyouhit range
+        Debug.Log("I hit this thing: " + hit.transform.name);
+    }
+}
